eb2aebac4a3d702c83a586f5e5613c038da12eb2
kristina1981a <kristina77@hotmail.co.uk>
2024-09-26 23:02:53 +0100

    js rocket

--- js/rocket.js
+++ js/rocket.js
@@ -1,37 +1,49 @@
 var timer = null;
 var countdownNumber = 10;
- 
+
 var changeState = function (state) {
-	document.body.className = 'body-state'+ state;
+	document.body.className = 'body-state'+state;
 	clearInterval(timer);
-	countdownNumber = 10
+	countdownNumber = 10;
 	document.getElementById('countdown').innerHTML = countdownNumber;
 
-    // countdown
-    if (state == 2) {
-    	timer = setInterval(function () {
-    		 countdownNumber = countdownNumber -1;
-             document.getElementById('countdown').innerHTML = countdownNumber;
-             
-            if (countdownNumber <= 0)   {
-               changeState(3);
-            };
-      	}, 500);
-    } else if (state == 3) {
-         var success = setTimeout(function () 
-             {
-                   var randomNumber = Math.round(Math.random()*10);
-                   
-                   console.log('randomNumber:',randomNumber)
+	// countdown
+	if (state == 2) {
+		timer = setInterval(function () {
+			countdownNumber = countdownNumber-1;
+			document.getElementById('countdown').innerHTML = countdownNumber;
+			
+			if (countdownNumber > 4 && countdownNumber <= 7) {
+				// be nervous
+				document.getElementById('nervous').className = 'nervous show';
+			} else{
+				document.getElementById('nervous').className = 'nervous';
+			}
 
-                   // success
-                   if (randomNumber > 5)  {
-                       changeState(4);
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				// can't wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else {
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
 
-                   }  else {
-                       changeState(5); //oh no!
-                   }
-                  
-         }, 2000);
-    };
+			if (countdownNumber <= 0) {
+				changeState(3);
+			};
+		}, 500);
+	} else if (state == 3) {
+		var success = setTimeout(function () {
+				var randomNumber = Math.round(Math.random()*10);
+
+				console.log('randomNumber:', randomNumber)
+
+				// succes
+				if (randomNumber > 2) {
+					changeState(4);
+
+				} else {
+					changeState(5); // oh no!
+				}
+		}, 2000);
+	};
 }
